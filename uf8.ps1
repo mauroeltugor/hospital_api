85fead9521d7dabeadd9cc809be1a877ebaa174d
dvdm12 <dvddavid677@gmail.com>
2025-04-25 14:30:32 -0500

    i've modified .gitignore

--- uf8.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem -Recurse -Filter *.java | ForEach-Object {
-    $content = Get-Content $_.FullName
-    $utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $false
-    [System.IO.File]::WriteAllLines($_.FullName, $content, $utf8NoBomEncoding)
-    Write-Host "Convertido: $($_.FullName)"
-}
-
-Write-Host "Conversion completa."
